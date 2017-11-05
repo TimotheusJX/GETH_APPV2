7c214de6cd090b25e0908f6d2127719c858e39f2
TimotheusJX <timotheus.jx@gmail.com>
2017-11-05 23:23:48 +0800

    minor changes to remove deprecated utility

--- src/pages/radio/radio.ts
+++ src/pages/radio/radio.ts
@@ -108,7 +108,6 @@ export class RadioPage {
     this.get_duration_interval = setInterval(function() {
       if(self.duration == -1) {
         self.duration = ~~(self.curr_playing_file.getDuration());  // make it an integer
-        // self.duration_string = self.fmtMSS(self.duration);   // replaced by the Angular DatePipe
       } else {
         self.curr_playing_file.stop();
         self.curr_playing_file.release();
@@ -203,22 +202,4 @@ export class RadioPage {
     }
   }
 
-
-  /*** utility functions ***/
-  
-  // this is replaced by the angular DatePipe:
-  // https://angular.io/api/common/DatePipe
-  fmtMSS(s){   // accepts seconds as Number or String. Returns m:ss
-    return( s -         // take value s and subtract (will try to convert String to Number)
-            ( s %= 60 ) // the new value of s, now holding the remainder of s divided by 60 
-                        // (will also try to convert String to Number)
-          ) / 60 + (    // and divide the resulting Number by 60 
-                        // (can never result in a fractional value = no need for rounding)
-                        // to which we concatenate a String (converts the Number to String)
-                        // who's reference is chosen by the conditional operator:
-            9 < s       // if    seconds is larger than 9
-            ? ':'       // then  we don't need to prepend a zero
-            : ':0'      // else  we do need to prepend a zero
-          ) + s ;       // and we add Number s to the string (converting it to String as well)
-  }
 }
