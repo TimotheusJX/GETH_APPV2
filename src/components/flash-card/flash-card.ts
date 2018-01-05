8bb1883debb5f58ab6c8029a39d4ad5b05e375f0
TimotheusJX <timotheus.jx@gmail.com>
2018-01-05 10:28:35 +0800

    touch up on flash card component

--- src/components/flash-card/flash-card.ts
+++ src/components/flash-card/flash-card.ts
@@ -1,27 +1,20 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-
+import { Component } from '@angular/core';
+ 
 @Component({
   selector: 'flash-card',
   templateUrl: 'flash-card.html'
 })
 export class FlashCardComponent {
-  @ViewChild('fcContainer') fcContainer;
-  @ViewChild('front') fcFront;
-  @ViewChild('back') fcBack;
-  toggled: boolean = false;
-
-  ngAfterViewChecked() {
-    const frontH = this.fcFront.nativeElement.querySelector('.fc-front').offsetHeight + 40;
-    const backH = this.fcBack.nativeElement.querySelector('.fc-back').offsetHeight + 40;
-    const h = ((frontH > backH) ? frontH :backH) + 'px';
-    this.fcContainer.nativeElement.style.height = h;
-    this.fcBack.nativeElement.style.height = h;
-    this.fcFront.nativeElement.style.height = h;
+ 
+  flipped: boolean = false;
+ 
+  constructor() {
+ 
   }
-
-  toggle() {
-    this.toggled = !this.toggled;
+ 
+  flip(){
+    this.flipped = !this.flipped;
   }
-
+ 
 }
  
